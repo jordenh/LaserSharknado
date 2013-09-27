8ae01f025d51762de68f963022b1a0832dc93928
Gord Larson <gordlarson@gmail.com>
2013-09-26 21:19:52 -0700

    Updated some types, can't check unsigned chars for being negative. Some mis-derefrencing as well. Can get to play loop but not play anything. Likely some problem reading the wav as I still get a really low value.

--- Software/Excercises/excercise2.h
+++
@@ -1,6 +0,0 @@
-#define MATRIX_SIZE 100
-
-void setupMatrixA(int matrix[MATRIX_SIZE][MATRIX_SIZE]);
-void setupMatrixB(int matrix[MATRIX_SIZE][MATRIX_SIZE]);
-void timeMatrixMultiply(void);
-void matrixMultiply(int a[MATRIX_SIZE][MATRIX_SIZE], int b[MATRIX_SIZE][MATRIX_SIZE]);
